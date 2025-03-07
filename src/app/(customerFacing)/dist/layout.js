660fc018786b6b64425911494880988a598b7f63
EndrexAkoto <martinakoto3@gmail.com>
2025-03-07 21:28:33 +0300

    creation of footer and alignment

--- src/app/(customerFacing)/dist/layout.js
+++ src/app/(customerFacing)/dist/layout.js
@@ -3,14 +3,16 @@ exports.__esModule = true;
 exports.dynamic = void 0;
 var Nav_1 = require("@/components/Nav");
 var Nav_2 = require("@/components/Nav");
+var Footer_1 = require("@/components/Footer");
 exports.dynamic = "force-dynamic";
 function Layout(_a) {
     var children = _a.children;
-    return (React.createElement(React.Fragment, null,
+    return (React.createElement("div", { className: "flex flex-col min-h-screen" },
         React.createElement(Nav_1["default"], null,
             React.createElement(Nav_2["default"], { href: "/" }, "Home"),
             React.createElement(Nav_2["default"], { href: "/products" }, "Products"),
             React.createElement(Nav_2["default"], { href: "/orders" }, "My Orders")),
-        React.createElement("div", { className: "container my-6" }, children)));
+        React.createElement("main", { className: "container my-6 flex-grow" }, children),
+        React.createElement(Footer_1["default"], null)));
 }
 exports["default"] = Layout;
--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -1,21 +1,19 @@
-import Nav from "@/components/Nav"
-import NavLink from "@/components/Nav"
+import Nav from "@/components/Nav";
+import NavLink from "@/components/Nav";
+import Footer from "@/components/Footer";
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
-export default function Layout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Nav>
         <NavLink href="/">Home</NavLink>
         <NavLink href="/products">Products</NavLink>
         <NavLink href="/orders">My Orders</NavLink>
       </Nav>
-      <div className="container my-6">{children}</div>
-    </>
-  )
+      <main className="container my-6 flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
 }
